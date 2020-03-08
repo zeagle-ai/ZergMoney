bf4b0b3c85c03877830236d20122aa774bb50caf
zeagle-ai <49596517+zeagle-ai@users.noreply.github.com>
2020-03-07 22:22:31 -0500

    added index and upload

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+﻿function transType() {
+    var btnid = document.getElementById("isDep");
+    if (btnid.getAttribute("value") == "true") {
+        btnid.setAttribute("value", "false");
+        btnid.innerHTML = "Withdraw";
+        btnid.className = "mdc-button mdc-button--unelevated filled-button--danger mdc-ripple-upgraded";
+    } else {
+        btnid.setAttribute("value", "true");
+        btnid.innerHTML = "Deposit";
+        btnid.className = "mdc-button mdc-button--unelevated filled-button--success mdc-ripple-upgraded";
+    };
+};
+
+function uploadFile() {
+    uploader.click();
+};
+
+function formSub() {
+    document.getElementById("transaction").submit();
+}
+
+function loadFile(event) {
+    formSub();
+};
